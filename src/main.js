458a5c65b385697601ef764e8db9f1350936e80f
Alexander Duka <alex.duka@maddev.com>
2017-03-05 21:33:50 +0200

    improved delimiter search and  file path selection

--- src/main.js
+++ src/main.js
@@ -14,42 +14,34 @@ server.on("connection", socket => {
 
   socket.on("data", data => {
     buffer = Buffer.concat([buffer, data]);
-    const stringifiedData = buffer.toString("utf-8");
 
-    if (stringifiedData.includes("\r\n\r\n")) {
-      const request = parseRequest(stringifiedData);
+    if (buffer.includes("\r\n\r\n")) {
+      const request = parseRequest(buffer.toString("utf-8"));
 
       global.console.log(request);
 
-      if (request.URL.lastIndexOf("/") < request.URL.lastIndexOf(".")) {
-        const fileName = request.URL.slice(
-          request.URL.lastIndexOf("/") + 1,
-          request.URL.length
-        );
+      const pathToFile = path.resolve(`./static${request.URL}`);
 
-        const pathToFile = path.resolve(`./static/${fileName}`);
+      fs.readFile(pathToFile, (err, fileData) => {
+        socket.write(`${request.Protocol} `);
+        if (err) {
+          const errorCode = errorCodes[err.code];
+          socket.write(`${errorCode} ${httpReasonPhrases[errorCode]}`);
+        }
+        socket.write("\r\n");
+        socket.write(`Date: ${new Date().toUTCString()}\r\n`);
+        socket.write("Server: Node Javascript.Ninja\r\n");
+        socket.write(`Content-Type: */*\r\n`);
 
-        fs.readFile(pathToFile, (err, fileData) => {
-          socket.write(`${request.Protocol} `);
-          if (err) {
-            const errorCode = errorCodes[err.code];
-            socket.write(`${errorCode} ${httpReasonPhrases[errorCode]}`);
-          }
-          socket.write("\r\n");
-          socket.write(`Date: ${new Date().toUTCString()}\r\n`);
-          socket.write("Server: Node Javascript.Ninja\r\n");
-          socket.write(`Content-Type: */*\r\n`);
-
-          if (!err) {
-            socket.write(`Content-Length: ${fileData.length}\r\n\r\n`);
-            socket.write(fileData);
-          } else {
-            socket.write("Connection: Closed");
-            socket.write(`Content-Length: 0\r\n\r\n`);
-          }
-          socket.end();
-        });
-      }
+        if (!err) {
+          socket.write(`Content-Length: ${fileData.length}\r\n\r\n`);
+          socket.write(fileData);
+        } else {
+          socket.write("Connection: Closed");
+          socket.write(`Content-Length: 0\r\n\r\n`);
+        }
+        socket.end();
+      });
     }
   });
 
