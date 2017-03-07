42bd46eceb79993a8705229c22489abc28be4022
Alexander Duka <alex.duka@maddev.com>
2017-03-07 14:32:03 +0200

    readFile and send data moved to services for faster switching between several types of implementation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import net from "net";
 import fs from "fs";
 import path from "path";
-import { parseRequest, httpReasonPhrases, errorCodes } from "./helpers";
+import handleRequest from "./services/task-0/using-readfile";
 
 fs.chmodSync(path.resolve(`./static/baz.html`), "000");
 global.console.log("Permissions to ./static/baz.html was set to 000.");
@@ -16,37 +16,12 @@ server.on("connection", socket => {
     buffer = Buffer.concat([buffer, data]);
 
     if (buffer.includes("\r\n\r\n")) {
-      const request = parseRequest(buffer.toString("utf-8"));
-
-      global.console.log(request);
-
-      const pathToFile = path.resolve(`./static${request.URL}`);
-
-      fs.readFile(pathToFile, (err, fileData) => {
-        socket.write(`${request.Protocol} `);
-        if (err) {
-          const errorCode = errorCodes[err.code];
-          socket.write(`${errorCode} ${httpReasonPhrases[errorCode]}`);
-        }
-        socket.write("\r\n");
-        socket.write(`Date: ${new Date().toUTCString()}\r\n`);
-        socket.write("Server: Node Javascript.Ninja\r\n");
-        socket.write(`Content-Type: */*\r\n`);
-
-        if (!err) {
-          socket.write(`Content-Length: ${fileData.length}\r\n\r\n`);
-          socket.write(fileData);
-        } else {
-          socket.write("Connection: Closed");
-          socket.write(`Content-Length: 0\r\n\r\n`);
-        }
-        socket.end();
-      });
+      handleRequest(buffer, socket);
     }
   });
 
   socket.on("error", err => {
-    global.console.log(err);
+    global.console.error(err);
   });
 });
 
